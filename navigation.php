94bcbfb167fc053b9bcc41b40c5a2a51bd3a7bf3
akmalwahyusiwi <167242922+akmalwahyusiwi@users.noreply.github.com>
2024-04-17 01:40:21 +0700

    Add files via upload

---
+++ navigation.php
@@ -0,0 +1,31 @@
+  <li class="header">MAIN NAVIGATION</li>
+           
+    <li class="treeview">
+      <a href="home.php">
+        <i class="fa fa-home"></i>
+        <span>Halaman utama</span>
+      </a>
+    </li>
+
+    <li class="treeview">
+      <a href="laundrytype.php">
+        <i class="fa fa-th-list"></i>
+        <span>Tipe Cucian</span>
+      </a>
+    </li>
+
+    <li class="treeview">
+      <a href="report.php">
+        <i class="fa fa-print"></i>
+        <span>Laporan</span>
+      </a>
+    </li>
+
+<li class="header">Pengaturan</li>
+	<li><a id="changePass" href="#"><i class="fa fa-circle-o text-yellow"></i> 
+		<span>Ubah Password</span></a>
+	</li>
+
+	<li><a href="logout.php"><i class="fa fa-circle-o text-red"></i> 
+		<span>Keluar</span></a>
+	</li>
